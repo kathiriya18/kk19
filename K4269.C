6315329f8794930eaa709ead987ca26745cc87ab
kathiriya18 <131762299+kathiriya18@users.noreply.github.com>
2023-05-11 15:47:33 +0530

    Add files via upload

---
+++ K4269.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int i=1,n;
+   crlscr();
+   printf("enter number=");
+   scanf("%d",&n);
+   while(i<=n)
+   {
+       printf("%d\n",i);
+       i+=2;
+   }
+   getch();
+
+}
+
+
+
+
+
+
+
+
+}
+
+
